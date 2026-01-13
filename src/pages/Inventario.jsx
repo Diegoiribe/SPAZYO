13ca783b2be7b40b9b240ba7a2ddceb55bcfd319
Diego Iribe <130528284+Diegoiribe@users.noreply.github.com>
2026-01-12 21:04:51 -0700

    change inventary ui/ux

--- src/pages/Inventario.jsx
+++ src/pages/Inventario.jsx
@@ -59,73 +59,74 @@ export const Inventario = () => {
       {!productState.isOpen ? (
         <>
           <p className="pb-5 text-2xl font-semibold uppercase">Inventario</p>
+          <div className="flex justify-between">
+            <div className="relative inline-flex items-center ">
+              <div className="inline-flex items-center gap-2 p-2 border rounded-md cursor-pointer bg-neutral-50 border-neutral-100">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-archive-icon lucide-archive text-neutral-500"
+                >
+                  <rect width="20" height="5" x="2" y="3" rx="1" />
+                  <path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8" />
+                  <path d="M10 12h4" />
+                </svg>
 
-          <div className="relative inline-flex items-center">
-            <div className="inline-flex items-center gap-2 p-2 border rounded-md cursor-pointer bg-neutral-50 border-neutral-100">
+                <select
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                  className="text-xs font-light uppercase bg-transparent outline-none appearance-none cursor-pointer text-neutral-500"
+                >
+                  {categories.map((cat) => (
+                    <option key={cat} value={cat}>
+                      {cat}
+                    </option>
+                  ))}
+                </select>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-chevron-down-icon lucide-chevron-down text-neutral-500"
+                >
+                  <path d="m6 9 6 6 6-6" />
+                </svg>
+              </div>
+            </div>
+            <div
+              className="inline-flex items-center gap-2 p-2 ml-2 border rounded-md cursor-pointer bg-neutral-50 border-neutral-100"
+              onClick={() => setIsCreateOpen(true)}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
+                width="14"
+                height="14"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-archive-icon lucide-archive text-neutral-500"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-plus-icon lucide-plus text-neutral-500"
               >
-                <rect width="20" height="5" x="2" y="3" rx="1" />
-                <path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8" />
-                <path d="M10 12h4" />
-              </svg>
-
-              <select
-                value={selectedCategory}
-                onChange={(e) => setSelectedCategory(e.target.value)}
-                className="text-xs font-light uppercase bg-transparent outline-none appearance-none cursor-pointer text-neutral-500"
-              >
-                {categories.map((cat) => (
-                  <option key={cat} value={cat}>
-                    {cat}
-                  </option>
-                ))}
-              </select>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-chevron-down-icon lucide-chevron-down text-neutral-500"
-              >
-                <path d="m6 9 6 6 6-6" />
+                <path d="M5 12h14" />
+                <path d="M12 5v14" />
               </svg>
             </div>
-          </div>
-          <div
-            className="inline-flex items-center gap-2 p-2 ml-2 border rounded-md cursor-pointer bg-neutral-50 border-neutral-100"
-            onClick={() => setIsCreateOpen(true)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-plus-icon lucide-plus text-neutral-500"
-            >
-              <path d="M5 12h14" />
-              <path d="M12 5v14" />
-            </svg>
           </div>
           <div className="mt-5 border rounded-md border-neutral-100">
             <div className="grid grid-cols-[40px_1fr_80px_10px] gap-4 px-3 py-3 text-xs font-light bg-neutral-50 border-b border-neutral-100 rounded-t-md text-neutral-500 uppercase">
@@ -143,7 +144,11 @@ export const Inventario = () => {
                 <div
                   onClick={() => openOrder(product.id)}
                   key={product.id}
-                  className="grid items-center grid-cols-[40px_1fr_80px_10px] gap-4 px-3 py-3 text-xs font-light border-b border-neutral-100 cursor-pointer"
+                  className={`grid items-center grid-cols-[40px_1fr_80px_10px] gap-4 px-3 py-3 text-xs  border-b border-neutral-100  cursor-pointer ${
+                    getTotalStockByProduct(product) < 5
+                      ? ' text-amber-400 font-medium'
+                      : 'text-black font-light'
+                  }`}
                 >
                   <p className="">{getTotalStockByProduct(product)}</p>
                   <p className="uppercase truncate">{product.name}</p>
